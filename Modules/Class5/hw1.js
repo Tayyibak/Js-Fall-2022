7fc51110f4cc3e790cf6ddeda3e6152d8b8b685e
Tayyiba kaisar <tayyibakaisar@Tayyibas-MBP.fios-router.home>
2022-10-07 18:54:04 -0400

    HW1

--- Modules/Class5/hw1.js
+++ Modules/Class5/hw1.js
@@ -28,62 +28,62 @@
 
  
 
- let fTemp1 = 90;
+let fTemp1 = 90;
 
- let cTemp1 = (fTemp1 - 32) * 5/9;
+let cTemp1 = (fTemp1 - 32) * 5/9;
 
- console.log(`\n${fTemp1}°F is equals to ${cTemp1}°C`);
+console.log(`\n${fTemp1}°F is equals to ${cTemp1}°C`);
 
 
 
 // Converting Fahrenheit to Kelvin down here
 
-  
-
- let kTemp = (fTemp1 + 459.67) * 5/9;
-
- console.log(`${fTemp1}°F is equal to ${kTemp}°K`)
-
  
 
- // Converting Celsius to Fahrenheit down here
+let kTemp = (fTemp1 + 459.67) * 5/9;
+
+console.log(`${fTemp1}°F is equal to ${kTemp}°K`)
 
 
 
- let cTemp2 = 25;
-
- let Ftemp2 = (cTemp2 * 1.8) + 32;
-
- console.log(`${cTemp2}°C is equal to ${Ftemp2}°F`);
+// Converting Celsius to Fahrenheit down here
 
 
 
- // Converting Celsius to Kelvin down here
+let cTemp2 = 25;
+
+let Ftemp2 = (cTemp2 * 1.8) + 32;
+
+console.log(`${cTemp2}°C is equal to ${Ftemp2}°F`);
 
 
 
- let Ktemp2 = cTemp2 + 273.15;
-
- console.log(`${cTemp2}°C is equal to ${Ktemp2}`);
+// Converting Celsius to Kelvin down here
 
 
 
- // Converting Kelvin to Fahrenheit down here
+let Ktemp2 = cTemp2 + 273.15;
+
+console.log(`${cTemp2}°C is equal to ${Ktemp2}`);
 
 
 
- let kTemp3 = 80;
-
- let Ftemp3 = (kTemp3 * 9/5) - 459.67
-
- console.log(`${kTemp3}°k is equal to ${Ftemp3}°F`);
+// Converting Kelvin to Fahrenheit down here
 
 
 
- // Converting Kelvin to celsius down here
+let kTemp3 = 80;
 
- let cTemp3 = kTemp3 - 273.15;
+let Ftemp3 = (kTemp3 * 9/5) - 459.67
 
- console.log(`${kTemp3}°K is equal to ${cTemp3}`);
+console.log(`${kTemp3}°k is equal to ${Ftemp3}°F`);
+
+
+
+// Converting Kelvin to celsius down here
+
+let cTemp3 = kTemp3 - 273.15;
+
+console.log(`${kTemp3}°K is equal to ${cTemp3}`);
 
  
